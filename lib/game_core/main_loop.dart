76e81c0c7edaef29263ad81ef0db14e19680420b
FixedIP <born.ru53@gmail.com>
2021-07-19 12:16:17 +0300

    Первичная инициализация

--- lib/game_core/main_loop.dart
+++ lib/game_core/main_loop.dart
@@ -1,9 +1,7 @@
-import 'dart:isolate';
-
 bool _running = true;
 
-void mainLoop(SendPort sendPort) async {
-  final double _fps = 50;
+void startLoop(){
+  final double _fps =50;
   final double _second = 1000;
   final double _updateTime = _second / _fps;
   double _updates = 0;
@@ -13,21 +11,20 @@ void mainLoop(SendPort sendPort) async {
   Stopwatch _timerWatch = Stopwatch();
   _timerWatch.start();
 
-  while (_running) {
-    if (_loopWatch.elapsedMilliseconds >= _updateTime) {
+  while (_running){
+    if (_loopWatch.elapsedMilliseconds > _updateTime){
       _updates++;
+      print("update");
       _loopWatch.reset();
-      sendPort.send(true);
     }
-
-    if (_timerWatch.elapsedMilliseconds > _second) {
+    if(_timerWatch.elapsedMilliseconds > _second){
       print("${DateTime.now()} FPS: $_updates");
       _updates = 0;
       _timerWatch.reset();
-    }
   }
 }
+}
 
-void stopLoop() {
+void stopLoop(){
   _running = false;
 }
